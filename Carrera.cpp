4620d023fbe8cb59ef4f57b00b39895ed52688c8
Thiago-Mateo <102254270+Thiago-Mateo@users.noreply.github.com>
2023-06-07 09:56:45 -0300

    Comenzamos a avanzar en el videojuego.

--- Carrera.cpp
+++ Carrera.cpp
@@ -1,6 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct mazo{
+	int numero_carta;
+	char palo[10];
+};
+
+void crear_mazo(struct mazo *cartas){
+	return;
+}
+
+void llenar_matriz(char carrera[5][21]){
+	for(int i = 0; i < 5; i++){
+		for(int j = 0; j < 21; j++){
+			if(i == 2 && (j+1) % 5 == 0) carrera[i][j] = '+';
+			else carrera[i][j] = '0';
+		}
+	}
+}
+
+void imprimir_carrera(char carrera[5][21]){
+	for(int i = 0; i < 5; i++){
+		for(int j = 0; j < 21; j++){
+			printf("%c", carrera[i][j]);
+		}
+		printf("\n");
+	}
+}
 int main(void){
-	printf("Hola alansito kun");
+	FILE *texto;
+	fopen("apuestas.txt", "w");
+	mazo *cartas = new mazo[48];
+	char carrera[5][21];
+	llenar_matriz(carrera);
+	imprimir_carrera(carrera);
 }
