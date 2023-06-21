a3654cde17ee6610ec043bc14968bc3e137716c9
Thiago-Mateo <102254270+Thiago-Mateo@users.noreply.github.com>
2023-06-21 09:57:10 -0300

    Actualizamos la funcion generar_cartas, creamos una nueva, seguimos avanzando en el juego.

--- Carrera.cpp
+++ Carrera.cpp
@@ -9,8 +9,6 @@ struct datos{
 	int apuesta;
 } jugador;
 
-
-
 void ingresar_jugadores(struct datos *jugador){
 printf("Ingrese el nombre del jugador: ");
 fgets(jugador->nom_apel, 100, stdin);
@@ -27,7 +25,6 @@ scanf("%d", &jugador->apuesta);
 	exit(1);
 	}
 	
-	
 	int flag=0;
 	
 	flag = fwrite(&apuestas, sizeof(struct datos), 1, apuestas);
@@ -39,48 +36,74 @@ scanf("%d", &jugador->apuesta);
 	fclose(apuestas);
 }
 
+void generar_carta_linea(int *ce,int *co,int *cb,int *cc){
+	tiro:
+	int r = (rand() %4);
+	switch(r){
+		case 0:
+			printf("Espada!\n");
+			*ce -= 1;	
+			break;
+		case 1:
+			printf("Oro!\n");
+			*co -= 1;
+			break;
+		case 2:
+			printf("Basto!\n");
+			*cb -= 1;
+			break;
+		case 3:
+			printf("Copa!\n");
+			*cc -= 1;
+			break;
+		default:
+			goto tiro;
+	}
+}
 
 void generar_carta(int *ce,int *co,int *cb,int *cc){
-	srand(time(NULL));
 	int comodin = 0;
 	tiro:
-	int r = (rand() %50);
-	if(r <= 12 && r > 0){
+	int r = (rand() %5);
+	switch(r){
+		case 0:
 			printf("Salio... Espada!\n");
 			if(comodin == 1) *ce += 2;
 			else *ce +=1;	
 			comodin = 0;
-		}
-	else if (r <= 24 && r > 12){
+			break;
+		case 1:
 			printf("Salio... Oro!\n");
 			if(comodin == 1) *co += 2;
 			else *co += 1;
 			comodin = 0;
-		}
-	else if (r <= 36 && r > 24){
+			break;
+		case 2:
 			printf("Salio... Basto!\n");
 			if(comodin == 1) *cb += 2;
 			else *cb += 1;
 			comodin = 0;
-		}
-	else if (r <= 48 && r > 36){
+			break;
+		case 3:
 			printf("Salio... Copa!\n");
 			if(comodin == 1) *cc += 2;
 			else *cc += 1;
 			comodin = 0;
-		}
-		else if(r > 48 && comodin == 0){
+			break;
+		case 4:
+			if(comodin == 1) goto tiro;
 			printf("Comodin!\n");
 			comodin = 1;
 			goto tiro;
-		}			
+			break;
+	}
 }
 
 void llenar_matriz(char carrera[5][21]){
 	for(int i = 0; i < 5; i++){
 		for(int j = 0; j < 21; j++){
-			if(i == 2 && (j+1) % 5 == 0) carrera[i][j] = '+';
-			else carrera[i][j] = '0';
+			if(i == 2 && (j+1) % 5 == 0) carrera[i][j] = '|';
+			else carrera[i][j] = '=';
 		}
 	}
 }
@@ -88,12 +111,12 @@ void llenar_matriz(char carrera[5][21]){
 void imprimir_matriz(char carrera[5][21], int ce, int co, int cb, int cc){
 	for(int i = 0; i < 5; i++){
 		for(int j = 0; j < 21; j++){
-				//Imprime Los Caballos
-			if (ce == j && i == 0)printf("%c", 006); // Caballo de Espada
-			if (co == j && i == 1)printf("%c", 003); // Caballo de Oro
-			if (cb == j && i == 3)printf("%c", 004); // Caballo de Basto
-			if (cc == j && i == 4)printf("%c", 005); // Caballo de Copa
-			else printf("%c", carrera[i][j]);
+			//Imprime Los Caballos
+			if (ce == j && i == 0)printf("%c ", 006); // Caballo de Espada
+			if (co == j && i == 1)printf("%c ", 003); // Caballo de Oro
+			if (cb == j && i == 3)printf("%c ", 004); // Caballo de Basto
+			if (cc == j && i == 4)printf("%c ", 005); // Caballo de Copa
+			else printf("%c ", carrera[i][j]);
 		}
 		printf("\n");
 	}
@@ -101,12 +124,12 @@ void imprimir_matriz(char carrera[5][21], int ce, int co, int cb, int cc){
 }
 
 void juego(char carrera[5][21]){
-	//Pone una semilla con raÃ­z time, asegurando que el numero generado siempre sea aleatorio.
-	srand(time(NULL));
 	//Cada variable representa la posicion de cada caballo, y comodin sirve como verificacion si ya salio un comodin.
 	int ce = 0, co = 0, cb = 0, cc = 0;
-	//Estas variables chequean si cada caballo pasÃ³ alguno de las cuatro lÃ­neas.
+	//Estas variables chequean si cada caballo pasÃƒÂ³ alguno de las cuatro lÃƒÂ­neas.
 	int ce1, ce2, ce3, ce4, co1, co2, co3, co4, cb1, cb2, cb3, cb4, cc1, cc2, cc3, cc4;
+	//Esta variable evita que se repita el evento de pasar un punto determinado
+	int ver = 0;
 	//El juego ocurre dentro de este while infinito.
 	while(1){
 		generar_carta(&ce, &co, &cb, &cc);
@@ -130,7 +153,7 @@ void juego(char carrera[5][21]){
 		else if(cc >= 9 && cc < 14 && cc2 != 1) cc2 = 1;
 		else if(cc >= 14 && cc < 19 && cc3 != 1) cc3 = 1;
 		else if(cc >= 19 && cc4 != 1) cc4 = 1;
-		//Llama a la funciÃ³n encargada de imprimir la matriz (valga la rebundancia).
+		//Llama a la funciÃƒÂ³n encargada de imprimir la matriz (valga la rebundancia).
 		imprimir_matriz(carrera, ce, co, cb, cc);
 		//Si alguno de los caballos llega al final de la matriz(Matriz de 21 lugares) gana.
 		if(ce == 20){
@@ -149,16 +172,19 @@ void juego(char carrera[5][21]){
 			printf("Gana el caballo de copa!");
 			return;
 		}
-		if(ce1 == 1 && co1 == 1 && cb1 == 1 && cc1 == 1){
+		if(ce1 == 1 && co1 == 1 && cb1 == 1 && cc1 == 1 && ver < 1){
 			printf("Ya todos los caballos pasaron la marca de la 5ta linea\nSe dara vuelta una carta, y el palo que salga retrocede un lugar\n");
 			printf("Presione enter para seguir\n");
 			while( getchar() != '\n' );
-			
+			generar_carta_linea(&ce, &co, &cb, &cc);
+			ver += 1;
+			//imprimir_matriz(carrera, ce, co, cb, cc);
 		}
 		//Espera tres segundos antes de empezar denuevo el loop
-		Sleep(3000);
+		Sleep(1500);
+		system("cls");
 		}
-	}
+	}}
 
 	//Menu del juego
 
@@ -176,6 +202,8 @@ void juego(char carrera[5][21]){
 }
 
 int main(void){
+	//Pone una semilla con raÃƒÂ­z time, asegurando que el numero generado siempre sea aleatorio.
+	srand(time(NULL));
 	/*FILE *apuestas;
 	fopen("apuestas.txt", "w+");*/	
 	char carrera[5][21];
@@ -183,6 +211,5 @@ int main(void){
 	llenar_matriz(carrera);
 	juego(carrera);
 	//menu();
-
 
 }
